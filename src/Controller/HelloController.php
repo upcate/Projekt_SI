e4c73619afa736ab18e8428ff902b15687398882
milosz <mil.swiatek1@gmail.com>
2022-04-30 11:01:26 +0000

    hello world testing

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class HelloController {
+    public function index(): Response {
+
+        return new Response('Hello World!');
+    }
+}
